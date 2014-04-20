d607180abd34d256245960efb908cfe1daab99bb
Erik Smartt <code@eriksmartt.com>
2014-04-20 18:28:55 -0500

    allows optionally splitting tags on spaces

--- sanity/split.py
+++ sanity/split.py
@@ -10,7 +10,7 @@ __doc__ = "A collection of misguided hacks."
 
 
 
-def taxonomy_tags(s):
+def taxonomy_tags(s, split_spaces=False):
     """
     >>> taxonomy_tags('hi there')
     ['hi there']
@@ -30,20 +30,31 @@ def taxonomy_tags(s):
     >>> taxonomy_tags('Hi, There friend, How goes it?')
     ['hi', 'there friend', 'how goes it']
 
+    >>> taxonomy_tags('hi there', split_spaces=True)
+    ['hi', 'there']
+
+    >>> taxonomy_tags('hi there, friend', split_spaces=True)
+    ['hi', 'there', 'friend']
+
     """
     if s is None:
         return None
 
     input = fmt.strip_tags(s)
 
+    delimeters = [';', '/', ':']
+
+    if split_spaces:
+        delimeters.append(' ')
+
     # Normalize delimeters
-    for delim in (';', '/', ':'):
+    for delim in delimeters:
         input = input.replace(delim, ',')
 
     # Stash intentional hyphens
     input = input.replace('-', '*')
 
-    tags = [fmt.slugify(fmt.strip_and_compact_str(tag)).replace('-', ' ').replace('*', '-') for tag in input.split(',')]
+    tags = [fmt.slugify(fmt.strip_and_compact_str(tag)).replace('-', ' ').replace('*', '-') for tag in input.split(',') if tag]
 
     return tags
 
