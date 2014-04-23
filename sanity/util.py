f219056678c38e7d63a0cc17c2afc2953cd379d0
Erik Smartt <code@eriksmartt.com>
2014-04-23 13:54:54 -0500

    adding line and char number output

--- sanity/util.py
+++ sanity/util.py
@@ -9,20 +9,29 @@ __doc__ = "A collection of misguided hacks."
 
 
 def find_nonascii_line_in_file(file_name):
-    count = 0
+    line_count = 1
+    char_count = 0
     error_count = 0
+
     with open(file_name, 'r') as fp:
         for line in fp.readlines():
             try:
+                char_count = 0
+                for c in line:
+                    if ord(c) > 128:
+                        print("Error on line: {c}, char: {n}, ord: {d}".format(c=line_count, n=char_count, d=ord(c)))
+
+                    char_count += 1
+
                 s = str(line)
+
             except:
-                print("Error on line: {c}".format(c=count))
+                print("Error on line: {c}".format(c=line_count))
                 error_count += 1
-            else:
-                count += 1
 
+            line_count += 1
 
-    print("Read: {c} lines".format(c=count))
+    print("Read: {c} lines".format(c=line_count))
     print("Found: {c} errors".format(c=error_count))
 
 
