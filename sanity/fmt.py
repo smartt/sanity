b7da55a076deb74578d20de9dddd7c2d70782476
Erik Smartt <code@eriksmartt.com>
2015-01-19 12:55:59 -0600

    spelling fix

--- sanity/fmt.py
+++ sanity/fmt.py
@@ -1100,7 +1100,7 @@ ASCII_MAP = [
 NUMBER_WORDS = (
     'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
 
-    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eightteen', 'nineteen',
+    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',
 
     'twenty', 'twenty-one', 'twenty-two', 'twenty-three', 'twenty-four', 'twenty-five',
     'twenty-six', 'twenty-seven', 'twenty-eight', 'twenty-nine',
