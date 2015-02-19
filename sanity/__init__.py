ba1ca4522a4b04adad09e0be335ac9975dde562e
Erik Smartt <code@eriksmartt.com>
2015-02-19 16:35:33 -0600

    better number-to-string'ing

---
+++ sanity/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__license__ = "MIT"
+__version__ = "0.3.5"
+__url__ = "http://github.com/smartt/sanity"
+__doc__ = "A collection of misguided hacks."
+
