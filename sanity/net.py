c4eef6a1e66429601fcf860329b1b1dd5f8deb3a
Erik Smartt <code@eriksmartt.com>
2015-03-30 13:12:42 -0400

    starting a net utility lib

---
+++ sanity/net.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import socket
+
+
+def ip_for_hostname(s):
+    """Yeah, this is a one-line function.
+
+    >>> ip_for_hostname(None)
+
+    >>> ip_for_hostname('google.com')
+    '173.194.115.9'
+
+    """
+    try:
+        return socket.gethostbyname(s)
+    except:
+        return None
+
+
+# --------------------------------------------------
+#               MAIN
+# --------------------------------------------------
+if __name__ == "__main__":
+    import doctest
+    print "[net.py] Testing..."
+    doctest.testmod()
+    print "Done."
