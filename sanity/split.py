bf46d33bfac327d0b18a35a8614c99b11b6b6963
Erik Smartt <code@eriksmartt.com>
2015-06-03 11:57:37 -0500

    adds function to split on sentences

--- sanity/split.py
+++ sanity/split.py
@@ -184,6 +184,148 @@ def taxonomy_tags(s, split_spaces=False):
 
     return tags
 
+
+def _find_troublemakers(s):
+    """
+    Scan the text and update the trouble_maker dictionary with
+    tricky patterns.  This is a black art...
+
+    We'll start with multi-letter (2+), all-caps abbreviations, like this:
+
+    >>> _find_troublemakers('The Hobbit was written by J.R.R. Tolkien.')
+    {'J.R.R.': 'nEWSLUGJRR'}
+
+    >>> _find_troublemakers('J.R.R. Tolkien changed society as we know it.')
+    {'J.R.R.': 'nEWSLUGJRR'}
+
+    >>> _find_troublemakers('The presentation by H.R. Roberts was wonderful.')
+    {'H.R.': 'nEWSLUGHR'}
+
+    >>> _find_troublemakers('H.R. Roberts called J.R.R. Tolkien and Mr. T. about the book.')
+    {'T.': 'nEWSLUGT', 'H.R.': 'nEWSLUGHR', 'J.R.R.': 'nEWSLUGJRR'}
+
+    # We also run into bad behavior with dates, like this:
+    >>> _find_troublemakers('Some txt (2004) is better than others.')
+    {'(2004)': 'nEWSLUGp2004'}
+
+    # This is a problematic sentence structure we'll deal with via a hack
+    >>> _find_troublemakers('Last year (again) Mike was better than others.')
+    {'n) M': 'nEWSLUGnPSM'}
+
+    # How about this mid-sentence punctuation?!
+    >>> _find_troublemakers('Last year really? was better than others.')
+    {'? w': 'nEWSLUGQPUNCw'}
+
+    """
+    d = dict()
+
+    # First, we're looking for initials
+    initials_matches = re.findall(r'\b(([A-Z]\.)+)', s)
+
+    for term in initials_matches:
+        d[term[0]] = 'nEWSLUG{st}'.format(st=term[0].replace('.', ''))
+
+    # Now look for years in parenthesis
+    paren_num_matches = re.findall(r'(\()([0-9]+?)(\))', s)
+
+    for term in paren_num_matches:
+        d["({x})".format(x=term[1])] = 'nEWSLUGp{x}'.format(x=term[1])
+
+    # Now look for capital words that follow words (or numbers) in parenthesis
+    blasted_capitals_after_parens = re.findall(r'([a-z0-9])\)\s([A-Z0-9])', s)
+    for term in blasted_capitals_after_parens:
+        d["{x}) {y}".format(x=term[0], y=term[1])] = 'nEWSLUG{x}PS{y}'.format(x=term[0], y=term[1])
+
+    # Now we go for question marks in the middle of a sentence.  Seriously.
+    mid_sentence_punctuation = re.findall(r'(\?)\s+([a-z])', s)
+    for term in mid_sentence_punctuation:
+        d["? {x}".format(x=term[1])] = 'nEWSLUGQPUNC{x}'.format(x=term[1])
+
+    return d
+
+
+def _slug_trouble_makers(s, patterns, verbose=False):
+    """
+    Swap trouble-maker's like 'Mr.' and 'Mrs.' for easily identifiable slugs.
+
+    >>> _slug_trouble_makers('Hello Mrs. Robinson.', patterns={'Mrs.': 'tMPSLUGMRS'})
+    'Hello tMPSLUGMRS Robinson.'
+
+    """
+    for k, v in patterns.items():
+        try:
+            s = s.replace(k, v)
+        except Exception as e:
+            if __debug__ and verbose:
+                print('ERROR: _slug_trouble_makers: {e}'.format(e=e))
+
+            continue
+
+    return s
+
+
+def _unslug_trouble_makers(s, patterns, verbose=False):
+    """
+    Swap trouble-maker's back in.
+
+    >>> _unslug_trouble_makers('Hello tMPSLUGMRS Robinson.', patterns={'Mrs.': 'tMPSLUGMRS'})
+    'Hello Mrs. Robinson.'
+
+    """
+    for k, v in patterns.items():
+        try:
+            s = s.replace(v, k)
+        except Exception as e:
+            if __debug__ and verbose:
+                print('ERROR: _unslug_trouble_makers: {e}'.format(e=e))
+            continue
+
+    return s
+
+
+def sentences(s):
+    """Split a block of text into sentences. Or try anyway.
+
+    >>> sentences('')
+    []
+
+    >>> sentences("Hello World!")
+    ['Hello World!']
+
+    >>> sentences("Hello World. I am a cat.")
+    ['Hello World.', 'I am a cat.']
+
+    """
+    # These are known trouble-maker patterns, given the period and the likelyhood that the next character
+    # will be capitalized.  In other words, to a basic parser, they look like the end of a sentence.
+    trouble_slugs = {
+        'Det.':  'tMPSLUGDET',
+        'Dr.':  'tMPSLUGDR',
+        'etc.': 'tMPSLUGetc',
+        'Etc.': 'tMPSLUGEtc',
+        'Mr.':  'tMPSLUGMISTER',
+        'Mrs.': 'tMPSLUGMRS',
+        'Ms.':  'tMPSLUGMS',
+        '(p.':  'tMPQUOTEPAGECOUNT',
+    }
+
+    new_troublemakers = _find_troublemakers(s)
+
+    for k, v in new_troublemakers:
+        trouble_slugs[k] = v
+
+    slugged_s = _slug_trouble_makers(s, trouble_slugs)
+
+    bits = re.findall(r'[\w\s\,\:\;]+[\.\!\?\)]\"?', slugged_s)
+
+    unslugged_lines = []
+
+    for txt in bits:
+        unslugged_lines.append(_unslug_trouble_makers(txt.strip(), trouble_slugs))
+
+    return unslugged_lines
+
+
 ## ---------------------
 if __name__ == "__main__":
     import doctest
